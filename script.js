74300a58414a70fb65d1dff1a1cf48428a55866e
ewankst <ewankerisit3@gmail.com>
2024-06-10 10:39:16 +0200

    navbar

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navItems = document.querySelectorAll('.nav-item');
+let linkItems = document.getElementsByTagName("li");
+let selector = document.getElementById("selector");
 
-    navItems.forEach(item => {
-        item.addEventListener('click', () => {
-            // Remove active class from all items
-            navItems.forEach(nav => nav.classList.remove('active'));
+selector.style.left = 3 * 50 - 50 + "px";
 
-            // Add active class to the clicked item
-            item.classList.add('active');
-        });
-    });
-});
+for (let linkItem of linkItems) {
+  linkItem.addEventListener("click", (e) => {
+    let listNum = e.target.dataset.list;
+    selector.style.left = listNum * 50 - 50 + "px";
+    event.target.classList.add("active");
+
+    // remove active class from other links
+    for (let linkItem of linkItems) {
+      if (linkItem.dataset.list !== listNum) {
+        linkItem.classList.remove("active");
+      }
+    }
+  });
+}
